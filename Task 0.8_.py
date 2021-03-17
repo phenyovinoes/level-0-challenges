93b9cffcaa2a041973254eef282155d4fb441b97
phenyovinoes <76746159+phenyovinoes@users.noreply.github.com>
2021-03-17 16:59:24 +0200

    updated task 0.8

--- Task 0.8_.py
+++ Task 0.8_.py
@@ -9,7 +9,7 @@ def converter_to_time(number):
         print(str(number // 60) + " hour " + str(number % 60) + " minute")
     elif number % 60 >= 2 and number >= 120:
         print(str(number // 60) + " hours " + str(number % 60) + " minutes")
-    elif number % 60 > 2 and number < 120:
+    elif number % 60 >= 2 and number < 120:
         print(str(number // 60) + " hour " + str(number % 60) + " minutes")
 
 
